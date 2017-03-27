78d3fd2c93545ab797eefbef04a23856fbb74f7e
LucioSt <luciostocosites@gmail.com>
2017-03-27 11:27:52 +0100

    Update exercise_collections_namedtuple.py

--- python_training_exercises/exercise_collections_namedtuple.py
+++ python_training_exercises/exercise_collections_namedtuple.py
@@ -1,53 +1,48 @@
-"""
-Exercícios - Python
+#!/usr/bin/env python
+#
+# Python Exercises
+# Shows the operation of a collections.namedtuple.
+# Use in classes that there are no methods. It works in place of:
+#
+# class Message(object):
+#    headers = []
+#    content = None
+#    from_addr = None
+#    to_addr = None
+#
+# Or then:
+#
+# class Message(object):
+#    def __init__(self, content, from_addr, to_addr):
+#        self.headers = []
+#        self.content = content
+#        self.from_addr = from_addr
+#        self.to_addr = to_addr
+#
+#    def add_header(self, info):
+#        self.headers.append(info)
+#
+# Use:
+#
+# >>> import collections
+# >>> Message = collections.namedtuple('Message', 'headers content from_addr to_addr')
+# >>> m = Message([], "Hello, server!", "me", "some.address.com")
+# >>> print m.content
+# Hello, server!
+# >>> m.headers.append("alguma informação")
+# >>> print m.headers
+# ["alguma informação"]
+# >>> m.to_addr = "localhost"
+# >>> print m.to_addr
+# localhost
+#
+# >>> Dados = collections.namedtuple('Cadastro', 'nome, sobrenome, endereco, fones')
+# >>> eu = Dados(nome='Vinicius',
+#...            sobrenome='Assef',
+#...            endereco='Rua 7 de setembro, 123',
+#...            fones=['3322-1100', '9988-7766'])
+#...
 
-Mostra o funcionamento de um collections.namedtuple.
-
-Uso em classes que não há metodos. Funciona no lugar de:
-
-class Message(object):
-    headers = []
-    content = None
-    from_addr = None
-    to_addr = None
-
-Ou então:
-
-class Message(object):
-    def __init__(self, content, from_addr, to_addr):
-        self.headers = []
-        self.content = content
-        self.from_addr = from_addr
-        self.to_addr = to_addr
-
-    def add_header(self, info):
-        self.headers.append(info)
-
-Uso:
-
->>> import collections
->>> Message = collections.namedtuple('Message', 'headers content from_addr to_addr')
->>> m = Message([], "Hello, server!", "me", "some.address.com")
->>> print m.content
-Hello, server!
->>> m.headers.append("alguma informação")
->>> print m.headers
-["alguma informação"]
->>> m.to_addr = "localhost"
->>> print m.to_addr
-localhost
-
-
-Ótima explicação - Para uso com BD - http://aprenda-python.blogspot.com.br/2013/03/named-tuples-ao-inves-de-classes-ou.html
-
->>> Dados = collections.namedtuple('Cadastro', 'nome, sobrenome, endereco, fones')
->>> eu = Dados(nome='Vinicius',
-...            sobrenome='Assef',
-...            endereco='Rua 7 de setembro, 123',
-...            fones=['3322-1100', '9988-7766'])
-...
-
-"""
 
 import collections
 
