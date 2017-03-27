89a898c559f879e11c539f1c92d0223cbf005556
LucioSt <luciostocosites@gmail.com>
2017-03-27 11:34:23 +0100

    Update exercise_swap_case.py

--- python_training_exercises/exercise_swap_case.py
+++ python_training_exercises/exercise_swap_case.py
@@ -1,20 +1,21 @@
-"""Sample code to read in test cases:
+#!/usr/bin/env python
+# Sample code to read in test cases:
+# 
+# INPUT SAMPLE:
+# 
+# Your program should accept as its first argument a path to a filename. Input example is the following
+# 
+# Hello world!
+# JavaScript language 1.8
+# A letter
+# OUTPUT SAMPLE:
+# 
+# Print results in the following way.
+# 
+# hELLO WORLD!
+# jAVAsCRIPT LANGUAGE 1.8
+# a LETTER
 
-INPUT SAMPLE:
-
-Your program should accept as its first argument a path to a filename. Input example is the following
-
-Hello world!
-JavaScript language 1.8
-A letter
-OUTPUT SAMPLE:
-
-Print results in the following way.
-
-hELLO WORLD!
-jAVAsCRIPT LANGUAGE 1.8
-a LETTER
-"""
 import sys
 
 with open(sys.argv[1]) as file: lines = [line.strip() for line in file]
