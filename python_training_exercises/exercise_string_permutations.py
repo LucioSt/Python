d519f3ecc99e45ea343681affb639771729af08a
LucioSt <luciostocosites@gmail.com>
2017-03-27 11:33:28 +0100

    Update exercise_string_permutations.py

--- python_training_exercises/exercise_string_permutations.py
+++ python_training_exercises/exercise_string_permutations.py
@@ -1,24 +1,24 @@
-"""Sample code to read in test cases:
-INPUT SAMPLE:
-
-Your program should accept a file as its first argument. The file contains input strings, one per line.
-
-For example:
-hat
-abc
-Zu6
-
-OUTPUT SAMPLE:
-
-Print to stdout the permutations of the string separated by comma, in alphabetical order.
-
-For example:
-
-aht,ath,hat,hta,tah,tha
-abc,acb,bac,bca,cab,cba
-6Zu,6uZ,Z6u,Zu6,u6Z,uZ6
-
-"""
+#!/usr/bin/env python
+#
+# Sample code to read in test cases:
+# INPUT SAMPLE:
+#
+# Your program should accept a file as its first argument. The file contains input strings, one per line.
+# 
+# For example:
+# hat
+# abc
+# Zu6
+# 
+# OUTPUT SAMPLE:
+# 
+# Print to stdout the permutations of the string separated by comma, in alphabetical order.
+# 
+# For example:
+# 
+# aht,ath,hat,hta,tah,tha
+# abc,acb,bac,bca,cab,cba
+# 6Zu,6uZ,Z6u,Zu6,u6Z,uZ6
 
 import sys
 from itertools import permutations
